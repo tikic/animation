09e910b834d65d3b64641cc641545a4fe688f8a8
unknown <tijana.gava@hotmail.com>
2022-11-30 23:57:27 +0100

    fifth sample

--- navigation/MainNavigation.tsx
+++ navigation/MainNavigation.tsx
@@ -1,24 +1,27 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import {createStackNavigator} from '@react-navigation/stack'
-import Animation_1 from '../screens/Animation_1';
 import {createDrawerNavigator} from '@react-navigation/drawer';
 import Home from '../screens/Home';
+
+import Animation_1 from '../screens/Animation_1';
 import Animation_2 from '../screens/Animation_2';
 import Animation_3 from '../screens/Animation_3';
 import Animation_4 from '../screens/Animation_4';
+import Animation_5 from '../screens/Animation_5';
 
 
 const Drawer = createDrawerNavigator();
 
 const DrawerNavigator = () => (
-    <Drawer.Navigator initialRouteName='Animation 4' screenOptions={{
+    <Drawer.Navigator initialRouteName='Animation 5' screenOptions={{
         headerShown: false
       }}>
         <Drawer.Screen name='Animation 1' component={Animation_1} />
         <Drawer.Screen name='Animation 2' component={Animation_2} />
         <Drawer.Screen name='Animation 3' component={Animation_3} />
         <Drawer.Screen name='Animation 4' component={Animation_4} />
+        <Drawer.Screen name='Animation 5' component={Animation_5} />
     </Drawer.Navigator>
 
 );
